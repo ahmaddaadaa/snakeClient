0e4fad079e349ec2ed48a2e4ef8724b84030c991
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-05 17:23:27 -0700

    updated

---
+++ input.js
@@ -0,0 +1,33 @@
+//const {connect} = require('./client');
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  console.log('key: ', key);
+  if (key === "\u0003") {
+    process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
+  } else if (key === "f") {
+    connection.write("Say: Ahmad");
+  }
+};
+
+module.exports = { setupInput };
---
+++ play.js
@@ -0,0 +1,8 @@
+const  {connect}   = require("./client");
+const  {setupInput} = require("./input");
+
+
+console.log("Connecting ...");
+const conn = connect();
+
+setupInput(conn);
